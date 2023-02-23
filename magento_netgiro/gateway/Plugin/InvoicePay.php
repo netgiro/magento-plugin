c244d1b7121d15927b6b5f53090d6e7d47226a1c
Stefán Örn Hrafnsson <stefan@smartmedia.is>
2023-02-23 09:50:00 +0000

    Attach Transaction to Invoice when it is generated

---
+++ magento_netgiro/gateway/Plugin/InvoicePay.php
@@ -0,0 +1,61 @@
+<?php
+namespace Netgiro\Gateway\Plugin;
+
+use Magento\Sales\Api\Data\InvoiceInterface;
+use Magento\Sales\Api\Data\TransactionInterface;
+use Magento\Sales\Api\InvoiceRepositoryInterface;
+use Magento\Sales\Api\TransactionRepositoryInterface;
+
+
+class InvoicePay
+{
+    /**
+     * Invoice repository
+     *
+     * @var InvoiceRepositoryInterface
+     */
+    protected $invoiceRepository;
+
+    /**
+     * Invoice repository
+     *
+     * @var TransactionRepositoryInterface
+     */
+    protected $transactionRepository;
+
+
+    /**
+     * Constructor
+     *
+     * @param InvoiceRepositoryInterface $invoiceRepository
+     */
+    public function __construct( InvoiceRepositoryInterface $invoiceRepository, TransactionRepositoryInterface $transactionRepository ) {
+        $this->invoiceRepository = $invoiceRepository;
+        $this->transactionRepository = $transactionRepository;
+    }
+
+    /**
+     * @param InvoiceRepositoryInterface $subject
+     * @param InvoiceInterface $invoice
+     * @return InvoiceInterface
+     */
+    public function afterSave($subject,  $invoice)
+    {
+        $orderId = $invoice->getOrderId();
+
+        $transaction = $this->transactionRepository->getByTransactionType(
+            TransactionInterface::TYPE_CAPTURE,
+            $orderId
+        );
+
+        //TODO  Villa geri ráð fyrir að það sé til transaction og 
+        //      gæti verið að rugla í enhverjum öðrum payment provider
+
+        $transactionId = $transaction->getId();
+        $invoice->setTransactionId($transactionId);
+        $this->invoiceRepository->save($invoice);
+        $this->sendToWebhook(['orderId' => $orderId, 'transactionId' => $transactionId ]);
+        return [$invoice];
+    }
+
+}
